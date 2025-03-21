e83be08c5338746835a15d81543988d404fc78ea
FittyVH <fittyvatshat@gmail.com>
2025-03-21 14:03:33 +0530

    sceneManagemnt

--- Assets/#Scripts/DialogueSystem.cs
+++ Assets/#Scripts/DialogueSystem.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DialogueSystem : MonoBehaviour
 {
     [SerializeField] AudioSource audioSource;
     [SerializeField] AudioClip pageTurnAudio;
+    // [SerializeField] GameObject lastImg;
 
     public GameObject[] comic;
     int i = 0;
@@ -23,7 +25,11 @@ public class DialogueSystem : MonoBehaviour
             comic[i].SetActive(false);
             i++;
             comic[i].SetActive(true);
-        }    
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
     }
 
     void PlayAudio()
