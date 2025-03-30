f0810767b8299a0d9525ca019cfcc4d53ce84f17
FittyVH <fittyvatshat@gmail.com>
2025-03-30 21:38:54 +0530

    Scene transition done

--- Assets/#Scripts/MainMenu.cs
+++ Assets/#Scripts/MainMenu.cs
@@ -5,12 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public Animator transitionAnimator;
+    [SerializeField] float transitionTime = 0.5f;
+
+    [SerializeField] AudioClip pageTurn;
+    [SerializeField] AudioSource secAudioSrc;
+
     public void OnPlayClicked()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        StartCoroutine(TransitionLoader());
+        secAudioSrc.clip = pageTurn;
+        secAudioSrc.Play();
     }
     public void OnQuitClicked()
     {
         Application.Quit();
+    }
+
+    IEnumerator TransitionLoader()
+    {
+        transitionAnimator.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
